90cf203c9d7fa5f9bee9cba3d041f98646f75797
andrevieira11 <andre.pa.vieira@hotmail.com>
2022-09-07 17:36:30 +0100

    add new config to prettier and elist

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import PropTypes from 'prop-types'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />
+}
+
+MyApp.propTypes = {
+	Component: PropTypes.elementType.isRequired,
+	pageProps: PropTypes.shape({
+		// your custom props here
+	}),
 }
 
 export default MyApp
--- pages/contest/[id]/submission/[submissionId].js
+++ pages/contest/[id]/submission/[submissionId].js
@@ -1,21 +1,22 @@
 export default function submission({ newID, newID2 }) {
-  return (
-    <div>
-      <p>{newID}</p>
-      <p>{newID2}</p>
-    </div>
-  );
+	return (
+		<div>
+			<p>{newID}</p>
+			<p>{newID2}</p>
+		</div>
+	)
 }
 
 export async function getServerSideProps(context) {
-  try {
-  } catch (err) {
-    console.log(err);
-  }
-  return {
-    props: {
-      newID: context.params.id,
-      newID2: context.params.submissionId,
-    },
-  };
+	try {
+		console.log(context.params)
+	} catch (err) {
+		console.log(err)
+	}
+	return {
+		props: {
+			newID: context.params.id,
+			newID2: context.params.submissionId,
+		},
+	}
 }
