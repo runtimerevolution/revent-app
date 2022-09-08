63d3ddc360d668e6d7f351fe40c95d281ea45534
andrevieira11 <andre.pa.vieira@hotmail.com>
2022-09-08 11:48:33 +0100

    change ident rules, remove ts files from being checked by eslint

--- components/BlurImage.js
+++ components/BlurImage.js
@@ -6,43 +6,43 @@ import PropTypes from 'prop-types'
 const { NEXT_PUBLIC_DECODE_PREFIX } = process.env
 
 export default function BlurImage({
-    contestId,
-    submissionId,
-    url,
-    user,
-    description,
+  contestId,
+  submissionId,
+  url,
+  user,
+  description,
 }) {
-    // TODO add loading image logic
-    // const [isLoading, setLoading] = useState(true)
-    if (!url.includes('data:image') && !url.includes('blob'))
-        url = NEXT_PUBLIC_DECODE_PREFIX + url
+  // TODO add loading image logic
+  // const [isLoading, setLoading] = useState(true)
+  if (!url.includes('data:image') && !url.includes('blob'))
+    url = NEXT_PUBLIC_DECODE_PREFIX + url
 
-    return (
-        <div className='w-full h-5/6'>
-            <Link href={`/contest/${contestId}/submission/${submissionId}`}>
-                <div className='w-full h-full bg-gray-200 rounded-lg overflow-hidden relative'>
-                    <Image
-                        alt=''
-                        src={url}
-                        sizes='100%'
-                        layout='fill'
-                        objectfit='contain'
-                        className='object-contain'
-                        //onLoadingComplete={() => setLoading(false)}
-                    />
-                </div>
-            </Link>
-
-            <h3 className='mt-4 text-sm text-gray-700'>{user}</h3>
-            <p className='mt-1 text-lg font-medium text-gray-900'>{description}</p>
+  return (
+    <div className='w-full h-5/6'>
+      <Link href={`/contest/${contestId}/submission/${submissionId}`}>
+        <div className='w-full h-full bg-gray-200 rounded-lg overflow-hidden relative'>
+          <Image
+            alt=''
+            src={url}
+            sizes='100%'
+            layout='fill'
+            objectfit='contain'
+            className='object-contain'
+            //onLoadingComplete={() => setLoading(false)}
+          />
         </div>
-    )
+      </Link>
+
+      <h3 className='mt-4 text-sm text-gray-700'>{user}</h3>
+      <p className='mt-1 text-lg font-medium text-gray-900'>{description}</p>
+    </div>
+  )
 }
 
 BlurImage.propTypes = {
-    contestId: PropTypes.number,
-    submissionId: PropTypes.number,
-    url: PropTypes.string,
-    user: PropTypes.string,
-    description: PropTypes.string,
+  contestId: PropTypes.number,
+  submissionId: PropTypes.number,
+  url: PropTypes.string,
+  user: PropTypes.string,
+  description: PropTypes.string,
 }
