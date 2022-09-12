9637d7b4974fc480a2ea694820a6bdeb8a9b28fd
Miguel Rodrigues <miguel.rodrigues@runtime-revolution.com>
2022-09-12 14:36:08 +0100

    added navbar first draft

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,14 @@
 import Footer from 'components/Footer'
 import Header from 'components/Header'
+import Navbar from 'components/Navbar'
 import PropTypes from 'prop-types'
 import styles from '../styles/Home.module.css'
 
 const Layout = ({ children }) => {
   return (
     <div className={styles.container}>
-      <Header />
+      {/* <Header /> */}
+      <Navbar />
       <main className={styles.main}>{children}</main>
       <Footer />
     </div>
