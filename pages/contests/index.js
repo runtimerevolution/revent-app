bc95cbf63f16e9b4e7e26fbd9cb6dcc1259fbf8c
Gabriel Esteves <gaby3_21@hotmail.com>
2022-10-26 08:41:13 +0100

    image click enabled

--- pages/contests/index.js
+++ pages/contests/index.js
@@ -1,26 +1,30 @@
 import { getContestList } from '/services/reventService.js'
 import PropTypes from 'prop-types'
+// import { BrowserRouter, Link } from 'react-router-dom'
+import Link from 'next/link'
 
 function RenderContest(props) {
   const { contest } = props
   return (
     <>
-      <div className='max-w-sm rounded overflow-hidden shadow-lg'>
-        <img
-          className='w-full'
-          src='https://www.thoughtco.com/thmb/LIkTVpCr3NAa7qJW7D57BXgRiJA=/3396x2547/smart/filters:no_upscale()/film-photography-592347645-59e4d0609abed500119e7b14.jpg'
-          alt=''
-        />
+      <Link href={'contest/' + contest.id}>
+        <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+          <img
+            className='w-full'
+            src='https://www.thoughtco.com/thmb/LIkTVpCr3NAa7qJW7D57BXgRiJA=/3396x2547/smart/filters:no_upscale()/film-photography-592347645-59e4d0609abed500119e7b14.jpg'
+            alt=''
+          />
 
-        <div className='px-6 py-4 text-center'>
-          <div className='font-bold text-xl mb-2'>{contest.name}</div>
-          <p className='text-gray-700 text-base'>{contest.description}</p>
-          <p className='text-gray-700 text-base'>
-            <a>End date: </a>
-            {contest.date_end.slice(0, 10)}
-          </p>
+          <div className='px-6 py-4 text-center'>
+            <div className='font-bold text-xl mb-2'>{contest.name}</div>
+            <p className='text-gray-700 text-base'>{contest.description}</p>
+            <p className='text-gray-700 text-base'>
+              <a>End date: </a>
+              {contest.date_end.slice(0, 10)}
+            </p>
+          </div>
         </div>
-      </div>
+      </Link>
     </>
   )
 }
@@ -28,6 +32,7 @@ function RenderContest(props) {
 export default function Home(props) {
   const { contestList } = props
   return (
+    // <BrowserRouter>
     <div className='p-8 bg-gray-300'>
       <div className='px-8'>
         <main className='min-h-screen py-8 px-20 flex-1 flex flex-col'>
@@ -39,6 +44,7 @@ export default function Home(props) {
         </main>
       </div>
     </div>
+    // {/* </BrowserRouter> */}
   )
 }
 
