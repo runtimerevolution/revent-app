f8a59dca1d67a63fc77aafd5cd98b341544e78c3
Miguel Rodrigues <miguel.rodrigues@runtime-revolution.com>
2023-05-17 12:06:11 +0100

    added notifications list first draft

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import Image from 'next/image'
+import { useState } from 'react'
 
 const Navbar = () => {
   const router = useRouter()
@@ -23,6 +24,14 @@ const Navbar = () => {
     router.pathname === '/contests'
       ? 'hover:bg-orange-700'
       : 'hover:bg-gray-700'
+
+  const [showNotifications, setShowNotifications] = useState<boolean>(false)
+
+  const [hasNotifications, setHasNotifications] = useState<boolean>(false)
+
+  const handleToggleNotifications = () => {
+    setShowNotifications((prevShowNotifications) => !prevShowNotifications)
+  }
 
   return (
     <nav className='bg-white-800 w-full'>
@@ -59,6 +68,38 @@ const Navbar = () => {
                 </a>
               </div>
             </div>
+          </div>
+          <div className='relative'>
+            <button
+              className='relative text-white focus:outline-none rounded-full p-2'
+              onClick={handleToggleNotifications}
+            >
+              <svg
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  d='M9 17.5V18.5C9 20.1569 10.3431 21 12 21C13.6569 21 15 20.1569 15 18.5V17.5M5.99999 8.5C5.99999 5.18629 8.68628 3.5 12 3.5C15.3137 3.5 18 5.18629 18 8.5C18 10.4392 18.705 12.6133 19.4316 14.3389C20.0348 15.7717 19.0222 17.5 17.4676 17.5H6.53237C4.97778 17.5 3.96518 15.7717 4.56842 14.3389C5.29493 12.6133 5.99999 10.4392 5.99999 8.5Z'
+                  stroke='#001A72'
+                  stroke-width='1.5'
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                />
+              </svg>
+              {hasNotifications && (
+                <span className='absolute top-0 right-0 bg-orange-500 text-white rounded-full w-4 h-4 flex items-center justify-center text-xs'></span>
+              )}
+            </button>
+            {showNotifications && (
+              <div className='absolute right-0 mt-2 bg-white text-gray-800 rounded-lg shadow-lg p-4'>
+                <p>Notification 1</p>
+                <p>Notification 2</p>
+                <p>Notification 3</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
