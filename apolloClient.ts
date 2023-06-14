431b81d9d527351537016e68878780e0ed3734ec
Miguel Rodrigues <miguel.rodrigues@runtime-revolution.com>
2023-06-14 10:08:02 +0100

    removed unused code

--- apolloClient.ts
+++ apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql/', // Replace with your Strawberry backend URL
+  uri: 'http://localhost:8000/graphql/',
   cache: new InMemoryCache(),
 })
 
