828249612e16c7add806b3740343336ddc20ae27
Miguel Rodrigues <miguel.rodrigues@runtime-revolution.com>
2023-06-19 16:19:35 +0100

    added error message interface

--- components/ErrorMessage.tsx
+++ components/ErrorMessage.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-export default function ErrorMessage({ error }) {
+interface ErrorMessageProps {
+  error: string
+}
+
+export default function ErrorMessage({ error }: ErrorMessageProps) {
   return (
     <>
       <p className='text-orange-500'>{error}</p>
