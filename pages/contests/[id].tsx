fbbf3630bff77c6a12526f6a69bdf35f166da58a
Miguel Rodrigues <miguel.rodrigues@runtime-revolution.com>
2023-06-22 12:07:53 +0100

    added static images to contest(draft, waiting on Submissions from BE)

--- pages/contests/[id].tsx
+++ pages/contests/[id].tsx
@@ -6,39 +6,64 @@ import { useQuery } from '@apollo/client'
 export default function DetailPage() {
   const router = useRouter()
   const { id } = router.query
-
-  let contestID: number | undefined
-  if (Array.isArray(id)) {
-    contestID = parseInt(id[0])
-  } else {
-    contestID = parseInt(id)
-  }
+  const contestID = parseInt(id as string, 10)
 
   const { loading, error, data } = useQuery(GET_CONTEST, {
     variables: { id: contestID },
   })
 
   const contestDetail = data?.contests[0]
-  console.log('contestDetail', contestDetail)
 
-  console.log(
-    'contestDetail?.cover_picture?.picture_path',
-    contestDetail?.cover_picture?.picture_path
-  )
+  const imageList = [
+    { id: 1, picture_path: '/images/contest.jpeg' },
+    { id: 2, picture_path: '/images/contest.jpeg' },
+    { id: 3, picture_path: '/images/contest.jpeg' },
+    { id: 4, picture_path: '/images/contest.jpeg' },
+    { id: 5, picture_path: '/images/contest.jpeg' },
+  ]
 
   return (
     <>
-      <div className='h-screen bg-gray-200'>
-        <div className='flex justify-center items-center bg-gray-200 p-4 rounded-lg flex items-center'>
-          <img
-            src={contestDetail?.cover_picture?.picture_path}
-            alt='Imagem'
-            className='w-48 h-auto'
-          />
-        </div>
-        <p className='ml-4'>{contestDetail?.title}</p>
-        <p className='ml-4'>{contestDetail?.description}</p>
-      </div>
+      {loading && <p>Loading</p>}
+      {error && <p>Error while retrieving the contest</p>}
+      {!loading && !error && (
+        <>
+          <div className='w-full flex justify-center  h-full bg-gray-200'>
+            <div className='bg-white p-8 rounded-lg shadow-lg'>
+              <div className='flex justify-center items-center'>
+                <img
+                  src={contestDetail?.cover_picture?.picture_path}
+                  alt='Imagem'
+                  className='w-60 h-auto'
+                />
+              </div>
+              <p className='text-center mt-4 text-2xl font-bold'>
+                {contestDetail?.title}
+              </p>
+              <p className='text-center mt-2 text-lg'>
+                {contestDetail?.description}
+              </p>
+              <div className='mt-6'>
+                <div className='flex flex-wrap mt-2 -mx-2'>
+                  {imageList.map((image) => (
+                    <div
+                      key={image.id}
+                      className='w-1/4 px-2 flex flex-col items-center mt-2'
+                    >
+                      <img
+                        src={image.picture_path}
+                        alt={`Image ${image.id}`}
+                        className='w-full h-auto'
+                      />
+                      <p className='mt-2 text-center'>{`Image ${image.id} Title`}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </>
   )
 }
