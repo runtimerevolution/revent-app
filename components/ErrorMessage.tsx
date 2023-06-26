2319b9597c3e36c5db9add636d7f354895d8101f
Miguel Rodrigues <104001268+miguelrodrigues22@users.noreply.github.com>
2023-06-26 11:09:18 +0100

    Create Contest Creation form (#61)

---
+++ components/ErrorMessage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface ErrorMessageProps {
+  error: string
+}
+
+export default function ErrorMessage({ error }: ErrorMessageProps) {
+  return <p className='text-orange-500'>{error}</p>
+}
---
+++ components/helpers/CustomFileInput.tsx
@@ -0,0 +1,49 @@
+import { useField } from 'formik'
+import ErrorMessage from '../ErrorMessage'
+
+interface Error {
+  title?: string
+  description?: string
+  prize?: string
+  cover_picture?: any
+  datesOption?: string
+  uploadPhaseDate?: any
+  votingPhaseDate?: any
+}
+interface CustomFileInputProps {
+  errors: Error
+  label: string
+  name: string
+}
+
+export default function CustomFileInput({
+  errors,
+  label,
+  ...props
+}: CustomFileInputProps) {
+  const [, , helpers] = useField(props.name)
+
+  const handleChange = (event) => {
+    const file = event.currentTarget.files[0]
+    helpers.setValue(file)
+  }
+
+  return (
+    <div>
+      <label>{label}</label>
+      <input
+        name={props.name}
+        type='file'
+        onChange={handleChange}
+        className='border border-black-500 w-full'
+      />
+
+      {errors?.cover_picture &&
+        (errors.cover_picture.type ? (
+          <ErrorMessage error={String(errors.cover_picture.type)} />
+        ) : (
+          <ErrorMessage error={String(errors.cover_picture)} />
+        ))}
+    </div>
+  )
+}
