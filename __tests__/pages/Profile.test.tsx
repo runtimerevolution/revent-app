9ac15399eb4ae158a6cbbd1c8fc89c0ff81048bd
Miguel Rodrigues <miguel.rodrigues@runtime-revolution.com>
2023-07-19 12:03:16 +0100

    tests updated

--- __tests__/pages/Profile.test.tsx
+++ __tests__/pages/Profile.test.tsx
@@ -3,19 +3,9 @@ import Profile from '../../pages/profile'
 import '@testing-library/jest-dom'
 import { render, screen, waitFor } from '@testing-library/react'
 
-test('renders example component', async () => {
-  render(<Profile />)
-  const textElement = screen.getByText('profile')
-  expect(textElement).toBeInTheDocument()
-
-  await waitFor(() => {
-    expect(textElement).toBeInTheDocument()
-  })
-})
-
 test('Check if specific text appears on the page', () => {
-  const { getByText } = render(<Profile />)
+  render(<Profile />)
   const expectedText = 'profile'
 
-  expect(getByText(expectedText)).toBeInTheDocument()
+  expect(screen.getByText(expectedText)).toBeInTheDocument()
 })
