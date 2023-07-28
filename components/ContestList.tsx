2d31e629c5456508ff6db4d6f02e182a79fadc46
Miguel Rodrigues <miguel.rodrigues@runtime-revolution.com>
2023-07-28 14:31:57 +0100

    dividers style updates

--- components/ContestList.tsx
+++ components/ContestList.tsx
@@ -17,20 +17,17 @@ export default function ContestList({ filteredContestList }) {
     <div>
       {openContests?.length > 0 && (
         <>
-          <div className='mt-2 w-full'>
-            <div className='flex items-center justify-center'>
-              <a className='flex items-center  custom-link'>
-                <Image
-                  src='/images/opencontests.svg'
-                  alt='plus'
-                  width={20}
-                  height={20}
-                  className='rounded-full'
-                />
-                <a className='ml-2'>Open Contests</a>
-              </a>
-            </div>
-            {/* <hr /> */}
+          <div className='mt-2'>
+            <a className='flex items-center custom-link'>
+              <Image
+                src='/images/opencontests.svg'
+                alt='plus'
+                width={20}
+                height={20}
+                className='rounded-full'
+              />
+              <a className='ml-1 w-72 text-gray-500'>Open Contests</a>
+            </a>
           </div>
           <div className='grid grid-cols-4 gap-4 mt-4'>
             {openContests?.map((contest) => (
@@ -42,19 +39,17 @@ export default function ContestList({ filteredContestList }) {
 
       {votingContests?.length > 0 && (
         <>
-          <div className='mt-2 w-full'>
-            <div className='flex items-center justify-center'>
-              <a className='flex items-center  custom-link'>
-                <Image
-                  src='/images/heart.svg'
-                  alt='plus'
-                  width={20}
-                  height={20}
-                  className='rounded-full'
-                />
-                <span className='ml-2'>Voting Contests</span>
-              </a>
-            </div>
+          <div className='mt-2'>
+            <a className='flex items-center custom-link'>
+              <Image
+                src='/images/heart.svg'
+                alt='plus'
+                width={20}
+                height={20}
+                className='rounded-full'
+              />
+              <a className='ml-1 w-72 text-gray-500'>Voting Phase</a>
+            </a>
           </div>
           <div className='grid grid-cols-4 gap-4 mt-4'>
             {votingContests?.map((contest) => (
@@ -66,19 +61,17 @@ export default function ContestList({ filteredContestList }) {
 
       {closedContests?.length > 0 && (
         <>
-          <div className='mt-2 w-full'>
-            <div className='flex items-center justify-center'>
-              <a className='flex items-center  custom-link'>
-                <Image
-                  src='/images/lock.svg'
-                  alt='plus'
-                  width={20}
-                  height={20}
-                  className='rounded-full'
-                />
-                <span className='ml-2'>Closed Contests</span>
-              </a>
-            </div>
+          <div className='mt-2'>
+            <a className='flex items-center custom-link'>
+              <Image
+                src='/images/lock.svg'
+                alt='plus'
+                width={20}
+                height={20}
+                className='rounded-full'
+              />
+              <a className='ml-1 w-72 text-gray-500'>Closed Contests</a>
+            </a>
           </div>
           <div className='grid grid-cols-4 gap-4 mt-4'>
             {closedContests?.map((contest) => (
